d2c0c88825c5b974f0ecec9c3086ef83c42a8c5b
Farah Jabri <farahjabri@hotmail.com>
2018-11-24 14:53:02 +0000

    moved consts

--- src/errorLogger.js
+++ src/errorLogger.js
@@ -1,23 +1,21 @@
-const greaterThanOne = "Amount must an integer greater than 1";
-const mustBeANumber = "Amount must be a number";
-const mustBeAnInteger = "Amount must be an integer";
-const insufficientFunds = "Insuficcient funds";
-
-
 function ErrorLogger() {}
 
 ErrorLogger.prototype.greaterThanOne = () => {
+  const greaterThanOne = "Amount must an integer greater than 1";
   return greaterThanOne;
 }
 
 ErrorLogger.prototype.mustBeANumber = () => {
+  const mustBeANumber = "Amount must be a number";
   return mustBeANumber;
 }
 
 ErrorLogger.prototype.mustBeAnInteger = () => {
+  const mustBeAnInteger = "Amount must be an integer";
   return mustBeAnInteger;
 }
 
 ErrorLogger.prototype.insufficientFunds = () => {
+  const insufficientFunds = "Insuficcient funds";
   return insufficientFunds;
 }
