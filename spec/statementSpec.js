fbbe7fb15ae6330699726bb6d80dc2ffb35f0214
Farah Jabri <farahjabri@hotmail.com>
2018-11-24 12:36:36 +0000

    ; added

--- spec/statementSpec.js
+++ spec/statementSpec.js
@@ -9,7 +9,7 @@ describe("Statement functions", () => {
     it("returns the formated statement with the heading", () => {
       expect(statement.printSatement()).toEqual([
         ['   Date     ||  Credit   ||  Debit   ||  Balance  ']
-      ])
+      ]);
     })
   })
 
@@ -21,7 +21,7 @@ describe("Statement functions", () => {
         [
           ['24/11/2018  ||  4.00  ||          ||  4.00']
         ]
-      ])
+      ]);
     })
   })
 
