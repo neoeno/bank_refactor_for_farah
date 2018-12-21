0c2069c1ab7a7b1a0f13862143f4fb6466e692f0
Farah Jabri <farahjabri@hotmail.com>
2018-12-21 13:04:43 +0000

    added eslint

--- src/appInterface.js
+++ src/appInterface.js
@@ -1,55 +1,55 @@
-function Bankapp() {
-  this._formatter = new Formatter();
-  this._errorLogger = new ErrorLogger();
+function Bankapp () {
+  this._formatter = new Formatter()
+  this._errorLogger = new ErrorLogger()
 
-  this._balance = 0;
-  this._depositAmount = 0;
-  this._withdrawAmount = 0;
+  this._balance = 0
+  this._depositAmount = 0
+  this._withdrawAmount = 0
 }
-Bankapp.prototype.showBalance = function() {
-  return this._balance;
+Bankapp.prototype.showBalance = function () {
+  return this._balance
 }
 
-Bankapp.prototype.deposit = function(amount) {
-  this._checkDepositAmount(amount);
+Bankapp.prototype.deposit = function (amount) {
+  this._checkDepositAmount(amount)
   if (this._depositAmount !== 0) {
     this._formatter.formatDepositTransactions(this._depositAmount, this._balance)
   }
 }
 
-Bankapp.prototype.withdraw = function(amount) {
-  this._checkWithdrawAmount(amount);
+Bankapp.prototype.withdraw = function (amount) {
+  this._checkWithdrawAmount(amount)
   if (this._withdrawAmount !== 0) {
     this._formatter.formatWithdrawTransactions(amount, this._balance)
   }
 }
 
-Bankapp.prototype.printStatement = function() {
-  return this._formatter.returnStatement();
+Bankapp.prototype.printStatement = function () {
+  return this._formatter.returnStatement()
 }
 
-Bankapp.prototype._checkDepositAmount = function(amount) {
+Bankapp.prototype._checkDepositAmount = function (amount) {
   if (amount < 1) {
-    console.log(this._errorLogger.greaterThanOne());
+    console.log(this._errorLogger.greaterThanOne())
   } else if (isNaN(amount)) {
-    console.log(this._errorLogger.mustBeANumber());
+    console.log(this._errorLogger.mustBeANumber())
   } else if (amount % 1 !== 0) {
-    console.log(this._errorLogger.mustBeAnInteger());
+    console.log(this._errorLogger.mustBeAnInteger())
   } else {
-    this._depositAmount = parseInt(amount);
+    this._depositAmount = parseInt(amount)
     this._balance = this._balance + this._depositAmount;
   }
 }
 
-Bankapp.prototype._checkWithdrawAmount = function(amount) {
+Bankapp.prototype._checkWithdrawAmount = function (amount) {
   if (amount > this._balance) {
-    console.log(this._errorLogger.insufficientFunds());
+    console.log(this._errorLogger.insufficientFunds())
   } else if (isNaN(amount)) {
-    console.log(this._errorLogger.mustBeANumber());
+    console.log(this._errorLogger.mustBeANumber())
   } else if (amount % 1 !== 0) {
-    console.log(this._errorLogger.mustBeAnInteger());
+    console.log(this._errorLogger.mustBeAnInteger())
   } else {
-    this._withdrawAmount = parseInt(amount);
-    this._balance = this._balance - this._withdrawAmount;
+    this._withdrawAmount = parseInt(amount)
+    this._balance = this._balance - this._withdrawAmount
   }
 }
---
+++ src/statement.js
@@ -0,0 +1,3 @@
+function Statement() {
+  this._statement = []
+}
