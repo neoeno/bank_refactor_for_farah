8e91bf48557f0440a5e5ee99d390b75758f2c8ce
Farah Jabri <farahjabri@hotmail.com>
2018-12-28 21:12:11 +0000

    format transactions specs added

--- spec/formatterSpec.js
+++ spec/formatterSpec.js
@@ -3,6 +3,22 @@ describe('Formatter functions', () => {
     formatter = new Formatter();
   });
 
+  describe('#formatDepositTransactions', () => {
+    it('formats the deposit transactions as per the requierments', () => {
+      formatter.formatDepositTransactions(44, 44);
+      let today = formatter._formatDate();
+      expect(formatter._transactionsHolder).toEqual([`${today}  ||  44.00  ||          ||  44.00`]);
+    });
+  });
+
+  describe('#formatWithdrawTransactions', () => {
+    it('formats the withdraw transactions as per the requierments', () => {
+      formatter.formatWithdrawTransactions(44, 44);
+      let today = formatter._formatDate();
+      expect(formatter._transactionsHolder).toEqual([`${today}  ||          ||  44.00  ||  44.00`]);
+    });
+  });
+
   describe('#sendFormattedTransaction', () => {
     it('returns the formatted deposit transaction to statement', () => {
       formatter.formatDepositTransactions(40, 80);
